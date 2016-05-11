2f51c6c4a920553a982bb677c49c773564541972
Wanding Zhou <wanding.zhou@vai.org>
2016-05-11 16:10:05 -0400

    bug fix to partial name matching in WRect

--- R/rect.R
+++ R/rect.R
@@ -1,6 +1,6 @@
 #' construct a WRect
-#' 
-#' @param x a plotting object or its name
+#'
+#' @param obj a plotting object or its name
 #' @param x.span x-axis/horizontal span (e.g., c(2,4))
 #' @param y.span y-axis/vertical span (e.g., c(5,9))
 #' @param color edge color
@@ -8,9 +8,9 @@
 #' @param fill fill color
 #' @param name name
 #' @return a WRect object
-#' 
+#'
 #' @export
-WRect <- function(x=NULL, x.span=NULL, y.span=NULL, color='black', lwd=3, fill=NA, name='') {
+WRect <- function(obj=NULL, x.span=NULL, y.span=NULL, color='black', lwd=3, fill=NA, name='') {
 
   rect <- lapply(formals(), eval)
   invisible(lapply(names(as.list(match.call()))[-1], function (nm) {
@@ -19,26 +19,27 @@ WRect <- function(x=NULL, x.span=NULL, y.span=NULL, color='black', lwd=3, fill=N
   class(rect) <- c('WRect', 'WAnnotate', 'WObject')
   force(rect)
   structure(function(group) {
-    if (is.null(rect$x))
-      rect$x <- group$children[[length(group$children)]]$name
 
-    x <- Resolve(rect$x, group)
-    if (is.null(x.span))
-      rect$x.span <- c(1, x$dm$nc)
-    if (is.null(y.span))
-      rect$y.span <- c(1, x$dm$nr)
+    if (is.null(rect$obj))
+      rect$obj <- group$children[[length(group$children)]]$name
+
+    obj <- Resolve(rect$obj, group)
+    if (is.null(rect$x.span))
+      rect$x.span <- c(1, obj$dm$nc)
+    if (is.null(rect$y.span))
+      rect$y.span <- c(1, obj$dm$nr)
 
     ## make inclusive
     rect$x.span[1] <- rect$x.span[1]-1
     rect$y.span[1] <- rect$y.span[1]-1
 
-    rect$x.span <- rect$x.span/x$dm$nc
-    rect$y.span <- (x$dm$nr-rect$y.span)/x$dm$nr
+    rect$x.span <- rect$x.span/obj$dm$nc
+    rect$y.span <- (obj$dm$nr-rect$y.span)/obj$dm$nr
 
-    rect$x.span[1] <- XToTop(x, group, rect$x.span[1])
-    rect$x.span[2] <- XToTop(x, group, rect$x.span[2])
-    rect$y.span[1] <- YToTop(x, group, rect$y.span[1])
-    rect$y.span[2] <- YToTop(x, group, rect$y.span[2])
+    rect$x.span[1] <- XToTop(obj, group, rect$x.span[1])
+    rect$x.span[2] <- XToTop(obj, group, rect$x.span[2])
+    rect$y.span[1] <- YToTop(obj, group, rect$y.span[1])
+    rect$y.span[2] <- YToTop(obj, group, rect$y.span[2])
 
     rect$dm <- WDim(rect$x.span[1], rect$y.span[2],
                     rect$x.span[2] - rect$x.span[1],
