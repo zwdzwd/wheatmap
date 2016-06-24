7d790bdd7d18c319eff4212b091901eaebf17050
Wanding Zhou <wanding.zhou@vai.org>
2016-06-24 15:55:47 -0400

    minor, message omitted

--- R/cluster.R
+++ R/cluster.R
@@ -6,12 +6,13 @@
 #' @param mat input matrix
 #' @param ... extra color bars or matrix that needs row reordered.
 #' @param hc.method method to use in hclust
+#' @param dist.method method to use in dist
 #' @return a list of clustered row, column and matrix
 #' @export
-row.cluster <- function(mat, ..., hc.method='ward.D2') {
-  d.row <- dist(mat)
+row.cluster <- function(mat, ..., hc.method='ward.D2', dist.method='euclidean') {
+  d.row <- dist(mat, method=dist.method)
   r <- list()
-  r$row.clust <- hclust(d.row)
+  r$row.clust <- hclust(d.row, method=hc.method)
   r$column.clust <- NULL
   r$mat <- mat[r$row.clust$order,]
   r$extra <- lapply(list(...), function(x) {
@@ -31,13 +32,14 @@ row.cluster <- function(mat, ..., hc.method='ward.D2') {
 #' @param mat input matrix
 #' @param ... extra color bars or matrix that needs column reordered
 #' @param hc.method method to use in hclust
+#' @param dist.method method to use in dist
 #' @return a list of clustered row, column and matrix
 #' @export
-column.cluster <- function(mat, ..., hc.method='ward.D2') {
-  d.column <- dist(t(mat))
+column.cluster <- function(mat, ..., hc.method='ward.D2', dist.method='euclidean') {
+  d.column <- dist(t(mat), method=dist.method)
   r <- list()
   r$row.clust <- NULL
-  r$column.clust <- hclust(d.column)
+  r$column.clust <- hclust(d.column, method=hc.method)
   r$mat <- mat[,r$column.clust$order]
   r$extra <- lapply(list(...), function(x) {
     if ('matrix' %in% class(x)) {
@@ -55,17 +57,18 @@ column.cluster <- function(mat, ..., hc.method='ward.D2') {
 #'
 #' @param mat input matrix
 #' @param hc.method method to use in hclust
+#' @param dist.method method to use in dist
 #' @param extra.row extra row reordering
 #' @param extra.column extra column reordering
 #' @return a list of clustered row, column and matrix
 #' @import stats
 #' @export
-both.cluster <- function(mat, extra.row=NULL, extra.column=NULL, hc.method='ward.D2') {
-  d.row <- dist(mat)
-  d.column <- dist(t(mat))
+both.cluster <- function(mat, extra.row=NULL, extra.column=NULL, hc.method='ward.D2', dist.method='euclidean') {
+  d.row <- dist(mat, method=dist.method)
+  d.column <- dist(t(mat), method=dist.method)
   r <- list()
-  r$row.clust <- hclust(d.row)
-  r$column.clust <- hclust(d.column)
+  r$row.clust <- hclust(d.row, method=hc.method)
+  r$column.clust <- hclust(d.column, method=hc.method)
   r$mat <- mat[r$row.clust$order, r$column.clust$order]
   r$extra <- c(
     lapply(extra.row, function(x) {
