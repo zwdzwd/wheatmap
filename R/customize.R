8f2b3ff6e464c4e06dfa486f10fb810e0ca6c972
Wanding Zhou <wanding.zhou@vai.org>
2017-11-12 11:11:45 -0500

    added mar all to wcustomize

--- R/customize.R
+++ R/customize.R
@@ -4,6 +4,7 @@
 #' @param mar.right right margin [0.03]
 #' @param mar.bottom bottom margin [0.03]
 #' @param mar.top top margin [0.03]
+#' @param mar margin in all directions [0.03]
 #' @return an object of class WCustomize
 #' @examples
 #' WHeatmap(matrix(c('fred','frank','brad',
@@ -11,13 +12,19 @@
 #'   WLegendV(NULL, RightOf(), label.fontsize = 20) +
 #'   WCustomize(mar.right=0.1)
 #' @export
-WCustomize <- function(mar.left=NULL, mar.right=NULL, mar.top=NULL, mar.bottom=NULL) {
+WCustomize <- function(mar.left=NULL, mar.right=NULL, mar.top=NULL, mar.bottom=NULL, mar=NULL) {
   wc <- list()
   invisible(lapply(names(as.list(match.call()))[-1], function (nm) {
     wc[[nm]] <<- get(nm)
   }))
   force(wc)
   structure(function(group) {
+    if (!is.null(wc[['mar']])) {
+      group$mar$left   <- wc[['mar']]
+      group$mar$right  <- wc[['mar']]
+      group$mar$top    <- wc[['mar']]
+      group$mar$bottom <- wc[['mar']]
+    }
     if (!is.null(wc$mar.left))
       group$mar$left <- wc$mar.left
     if (!is.null(wc$mar.right))
@@ -26,6 +33,7 @@ WCustomize <- function(mar.left=NULL, mar.right=NULL, mar.top=NULL, mar.bottom=N
       group$mar$top <- wc$mar.top
     if (!is.null(wc$mar.bottom))
       group$mar$bottom <- wc$mar.bottom
+
     group
   }, class = c('WCustomize', 'WObject'))
 }
